db1414ee31e77ab5cd8b2428ab74e7ef52c12494
Philip Diffenderfer <pdiffenderfer@gmail.com>
2016-05-31 00:39:04 -0400

    Restored uuid functionality previously removed advised by jshint

--- build/rekord.js
+++ build/rekord.js
@@ -577,7 +577,7 @@ function uuid()
 
 function S4()
 {
-  return ((1+Math.random())*0x10000).toString(16).substring(1);
+  return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
 }
 
 
--- src/lib/functions/common.js
+++ src/lib/functions/common.js
@@ -298,7 +298,7 @@ function uuid()
 
 function S4()
 {
-  return ((1+Math.random())*0x10000).toString(16).substring(1);
+  return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
 }
 
 
