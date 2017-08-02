2bb6d76520963cf792f21dd34722d3042476f28c
Philip Diffenderfer <pdiffenderfer@gmail.com>
2017-08-01 23:22:18 -0400

    Updated Mass Releasing

--- scripts/apply-versions.sh
+++ scripts/apply-versions.sh
@@ -25,14 +25,13 @@ then
 fi
 
 # ################################
-# Variables
+# Configuration
 # ################################
 
-search='("version":[[:space:]]*").+(")'
-rekord='("rekord":[[:space:]]*).+("")'
-replace="\1${version}\2"
-approx="\1~${version}\2"
-chosen="rekord-apply-version"
+chosen="apply-version.tmp"
+root="rekord"
+prefix="rekord"
+repos="https://api.github.com/repos/Rekord"
 
 # ################################
 # Functions
@@ -42,24 +41,28 @@ chosen="rekord-apply-version"
 function iterate_projects() {
   question="$1"
   function="$2"
-  read -p "${question}? (y/n): " -n 1 -r
-  echo
+  REPLY="Y"
+  if [ -n "${question}" ]; then
+    read -p "${question}? (y/n): " -n 1 -r
+    echo
+  fi
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    cd "$base"
-    for dir in rekord*; do
-      if [ -f "${dir}/${chosen}" ]; then
-        cd "${base}/${dir}"
+    cd "${base}"
+    for dir in ${prefix}* ; do
+      cd "${dir}"
+      if [ -f "${chosen}" ]; then
         eval ${function} "${dir}"
       fi
+      cd "${base}"
     done
   fi
 }
 
-# iterate_project_file( question, function, file ) // for each chosen project that has the given file, invoke function ${file} ${dir}
+# iterate_project_file( question, function, filename ) // for each chosen project that has the given file, invoke function ${file} ${dir}
 function iterate_project_file() {
   question="$1"
   function="$2"
-  file="$3"
+  filename="$3"
   REPLY="Y"
   if [ -n "${question}" ]; then
     read -p "${question}? (y/n): " -n 1 -r
@@ -67,9 +70,9 @@ function iterate_project_file() {
   fi
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     cd "$base"
-    for dir in rekord*; do
+    for dir in ${base}/${prefix}* ; do
       if [ -f "${dir}/${chosen}" ]; then
-        instance="${dir}/${file}"
+        instance="${dir}/${filename}"
         if [ -f "${instance}" ]; then
           eval ${function} "${instance}" "${dir}"
         fi
@@ -81,23 +84,33 @@ function iterate_project_file() {
 # update_version ( file ) // Update version property in JSON file
 function update_version() {
   file="$1"
+  search="(\"version\":[[:space:]]*\").+(\")"
+  replace="\1${version}\2"
   sed -i ".tmp" -E "s/${search}/${replace}/g" "${file}"
   rm "${file}.tmp"
 }
 
-# update_dependency( file ) // Update rekord property in dependencies in JSON file
+# update_dependency( file ) // Update project property in dependencies in JSON file
 function update_dependency() {
   file="$1"
-  sed -i ".tmp" -E "s/${rekord}/${approx}/g" "${file}"
+  search="(\"${root}\":[[:space:]]*\").+(\")"
+  replace="\1~${version}\2"
+  sed -i ".tmp" -E "s/${search}/${replace}/g" "${file}"
   rm "${file}.tmp"
 }
 
-# run_build() // Run the build process for the current project
+# run_build( project ) // Run the build process for the current project
 function run_build() {
-  gulp
+  project="$1"
+  if [ -f "Gulpfile.js" ]; then
+    if ! gulp ; then
+      echo "The build process for ${project} failed!"
+      exit 1
+    fi
+  fi
 }
 
-# commit_build() // Commit the changed files locally and remotely
+# commit_build( project ) // Commit the changed files locally and remotely
 function commit_build() {
   git add -A
   git commit -m "Build for ${version}"
@@ -106,38 +119,43 @@ function commit_build() {
 
 # add_release( project ) // Adds
 function add_release() {
-  dir="$1"
+  project="$1"
   if [ -z "${access_token}" ]; then
     echo "Access token is required for adding releases"
     exit 1
   fi
-  API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "Release of version %s","draft": false,"prerelease": false}' ${version} ${version} ${version})
-  curl --data "$API_JSON" https://api.github.com/repos/Rekord/${dir}/releases?access_token=${access_token}
+  API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master","name": "Release %s","body": "Automatic release with Rekord %s","draft": false,"prerelease": false}' ${version} ${version} ${version})
+  curl --data "$API_JSON" ${repos}/${project}/releases?access_token=${access_token}
 }
 
-# publish_node() // Publish to NPM
+# publish_node( project ) // Publish to NPM
 function publish_node() {
   npm publish
 }
 
 # choose_projects() // Allow user to choose which projects we're going to work with
 function choose_projects() {
-  for dir in rekord*; do
+  for dir in ${prefix}*; do
     read -p "${dir}? (y/n): " -n 1 -r
     echo
+    file="${dir}/${chosen}"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-      touch "${dir}/${chosen}"
+      touch "${file}"
+    else
+      if [ -f "${file}" ]; then
+        rm "${file}"
+      fi
     fi
   done
 }
 
-# ignore_chose() // Add chosen markers to gitignore
+# ignore_chose( file ) // Add chosen markers to gitignore
 function ignore_chosen() {
   file="$1"
-  grep -qF "${chosen}" "${file}" || echo "${chosen}" >> "${file}"
+  grep -qF "${chosen}" "${file}" || echo -e "\n${chosen}" >> "${file}"
 }
 
-# remove_file() // Removes file
+# remove_file( file ) // Removes file
 function remove_file() {
   file="$1"
   rm "${file}"
@@ -155,9 +173,9 @@ iterate_project_file "Update bower.json files" update_version "bower.json"
 
 iterate_project_file "Update package.json files" update_version "package.json"
 
-iterate_project_file "Update rekord dependency (bower)" update_dependency "bower.json"
+iterate_project_file "Update dependency (bower)" update_dependency "bower.json"
 
-iterate_project_file "Update rekord dependency (npm)" update_dependency "package.json"
+iterate_project_file "Update dependency (npm)" update_dependency "package.json"
 
 iterate_projects "Run builds" run_build
 
@@ -167,4 +185,4 @@ iterate_projects "Create releases" add_release
 
 iterate_projects "Publish to NPM" publish_node
 
-iterate_project_file "" remove_file "rekord-apply-version"
+iterate_project_file "" remove_file "${chosen}"
